60f7c66695db1042725b10bcc0a7dd09583c6bfc
Sergei <radchenkovsa@yandex.ru>
2023-09-15 22:10:04 +0300

    Task54

---
+++ Task54/Program.cs
@@ -0,0 +1,99 @@
+﻿/* Задача 54: Задайте двумерный массив. Напишите программу, 
+которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2 */
+
+int InputNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int[,] Create2DArray(int rows, int columns)
+{
+    return new int[rows, columns];
+}
+
+void Fill2DArray(int[,] array)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = rnd.Next(1, 10);
+}
+
+int[] CopyArray(int[,] array)
+{
+    int[] ar = new int[array.Length];
+    int index = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            ar[index] = array[i, j];
+            index++;
+        }
+    }
+    return ar;
+}
+
+void SortArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] > array[i])
+            {
+                int temp = array[j];
+                array[j] = array[i];
+                array[i] = temp;
+            }
+        }
+    }
+}
+
+void Fill2DArray2(int[,] array, int[] ar)
+{
+    int number = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (number < ar.Length)
+            {
+                array[i, j] = ar[number];
+                number++;
+            }
+        }
+    }
+}
+
+void Print2DArray(int[,] array)
+{
+    Console.WriteLine();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j]}\t");
+        Console.WriteLine();
+    }
+}
+
+int rows = InputNum("Введите количество строк: ");
+int columns = InputNum("Введите количество столцов: ");
+
+int[,] myArray = Create2DArray(rows, columns);
+
+Fill2DArray(myArray);
+Print2DArray(myArray);
+int[] newArray = CopyArray(myArray);
+SortArray(newArray);
+Fill2DArray2(myArray, newArray);
+Print2DArray(myArray);
